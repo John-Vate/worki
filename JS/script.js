a0f3e9a768aba3510b1000fb75aad03b655be6f6
John-Vate <jvitor.depaula@gmail.com>
2021-12-24 17:15:55 -0300

    confirmar em 'main'

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,15 @@
-let logobtn = document.querySelector("#logobtn")
-let nav = document.querySelector("#l-nav")
+let btntroca= document.querySelector("#btntroca")
+let cardsig= document.querySelector("#cardsig")
+
+btntroca.addEventListener("click", ()=>{
+    cardsig.classList.toggle("removecardsig")
+})
+
+let logobtn= document.querySelector("#logobtn")
+let nav= document.querySelector("#l-nav")
 
 logobtn.addEventListener("click", ()=>{
-    nav.classList.toggle("active")
+    nav.classList.toggle("activenav")
 })
+
+let lheader= document.querySelector("#l-header")
---
+++ JS/validacion.js
@@ -0,0 +1,54 @@
+
+			let name= document.querySelector("#email")
+			let password= document.querySelector("#password")
+			let passwordrepeat = document.querySelector("#passwordrepeat")
+			var re = /\S+@\S+\.\S+/;
+
+			name.addEventListener("keyup", ()=>{
+			if(re.test(name.value)){
+			    name.style.borderBottom ="1px solid green"
+			} else{
+			    name.style.borderBottom ="1px solid red"
+				}
+			})
+
+
+			let A = /[A-Z]/;
+			let a = /[a-z]/;
+			let number = /[0-9]/;
+			let charx= /[!|@|#|$|%|^|&|*|(|)|-|_]/;
+
+			password.addEventListener("keyup", ()=>{
+			if(A.test(password.value) &&
+			a.test(password.value) &&
+			number.test(password.value) &&
+			charx.test(password.value) &&
+			password.value.length >= "8"){
+			password.style.borderBottom= "1px solid green"
+			} else {
+                password.style.borderBottom= "1px solid red"
+			}
+			});
+			let button = document.querySelector("#button")
+			passwordrepeat.addEventListener("keyup", ()=>{
+			if(passwordrepeat.value == password.value &&
+			labelname.style.color =="green" &&
+			labeluser.style.color == "green"){
+			labelpasswordrepeat.style.color = "green"
+			button.style.display = "block"
+			}else {
+			labelpasswordrepeat.style.color = "red"
+    
+			}
+			});
+
+			let user = document.querySelector("#user");
+			let labeluser = document.querySelector("#labeluser");
+
+			user.addEventListener("keyup", ()=>{
+			if(user.value.length >= "3"){
+			labeluser.style.color = "green"
+			}else{
+			labeluser.style.color = "red"
+			}
+			});
