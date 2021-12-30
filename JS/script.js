8693d18259b3eb65bba18f30cb23476f5cfa6567
John-Vate <jvitor.depaula@gmail.com>
2021-12-30 12:42:23 -0300

    confirmar em 'main'

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,5 @@
-let btntroca= document.querySelector("#btntroca")
-let cardsig= document.querySelector("#cardsig")
-let btnentrar= document.querySelector("#btnentrar")
-let before= document.querySelector("#before")
-let cardlog= document.querySelector("#cardlog")
-let btntrocavolta= document.querySelector("#btntrocavolta")
-
-btntroca.addEventListener("click", ()=>{
-    cardsig.style.left="1000px"
-    cardlog.style.left= "0px"
-})
-
-btntrocavolta.addEventListener("click", ()=>{
-    cardsig.style.left="0px"
-    cardlog.style.left= "-1000px"
-})
-
-let logobtn= document.querySelector("#logobtn")
-let nav= document.querySelector("#l-nav")
 
 logobtn.addEventListener("click", ()=>{
     nav.classList.toggle("activenav")
 })
 
-let lheader= document.querySelector("#l-header")
--- JS/validacion.js
+++ JS/validacion.js
@@ -1,55 +1,48 @@
 
-			let name= document.querySelector("#email")
-			let password= document.querySelector("#password")
-			let passwordrepeat = document.querySelector("#passwordrepeat")
-			var re = /\S+@\S+\.\S+/;
-
-			name.addEventListener("keyup", ()=>{
-			if(re.test(name.value)){
-			    name.style.borderBottom ="1px solid green"
-			} else{
-			    name.style.borderBottom ="1px solid red"
-				}
-			})
 
 
-			let A = /[A-Z]/;
-			let a = /[a-z]/;
-			let number = /[0-9]/;
-			let charx= /[!|@|#|$|%|^|&|*|(|)|-|_]/;
+name.addEventListener("keyup", ()=>{
+if(re.test(name.value)){
+name.style.borderBottom ="1px solid green"
+} else{
+name.style.borderBottom ="1px solid red"
+}
+})
 
-			password.addEventListener("keyup", ()=>{
-			if(A.test(password.value) &&
-			a.test(password.value) &&
-			number.test(password.value) &&
-			charx.test(password.value) &&
-			password.value.length >= "8"){
-			password.style.borderBottom= "1px solid green"
-            btnentrar.style.opacity= "1";
-        } else {
-                password.style.borderBottom= "1px solid red"
-			}
-			});
-			let button = document.querySelector("#button")
-			passwordrepeat.addEventListener("keyup", ()=>{
-			if(passwordrepeat.value == password.value &&
-			labelname.style.color =="green" &&
-			labeluser.style.color == "green"){
-			labelpasswordrepeat.style.color = "green"
-			button.style.display = "block"
-			}else {
-			labelpasswordrepeat.style.color = "red"
-    
-			}
-			});
 
-			let user = document.querySelector("#user");
-			let labeluser = document.querySelector("#labeluser");
 
-			user.addEventListener("keyup", ()=>{
-			if(user.value.length >= "3"){
-			labeluser.style.color = "green"
-			}else{
-			labeluser.style.color = "red"
-			}
-			});
+password.addEventListener("keyup", ()=>{
+if(A.test(password.value) &&
+a.test(password.value) &&
+number.test(password.value) &&
+charx.test(password.value) &&
+password.value.length >= "8" &&
+name.style.borderBottom =="1px solid green"){
+password.style.borderBottom= "1px solid green"
+btnentrar.style.opacity= "1";
+} else {
+password.style.borderBottom= "1px solid red"
+}
+});
+
+passwordrepeat.addEventListener("keyup", ()=>{
+if(passwordrepeat.value == password.value &&
+labelname.style.color =="green" &&
+labeluser.style.color == "green"){
+labelpasswordrepeat.style.color = "green"
+button.style.display = "block"
+}else {
+labelpasswordrepeat.style.color = "red"
+
+}
+});
+
+
+
+user.addEventListener("keyup", ()=>{
+if(user.value.length >= "3"){
+labeluser.style.color = "green"
+}else{
+labeluser.style.color = "red"
+}
+});
