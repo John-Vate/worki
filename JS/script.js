e4f31e5c8e1b7a9221a3b0885db123e6d3980564
john-vate <jvitor.depaula@gmail.com>
2022-03-10 17:18:47 -0300

    1.0

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,15 @@ logobtn.addEventListener("click", ()=>{
     nav.classList.toggle("activenav")
 })
 
+var perfilname = "John"
+
+window.onload = perfil()
+
+function perfil(){
+    let perfilIcon= document.querySelector("#perfil")
+    let perfil= document.createElement("div")
+    perfil.innerText = perfilname
+    
+    perfilIcon.appendChild(perfil)
+    perfil.classList.add("perfiltag")
+}
